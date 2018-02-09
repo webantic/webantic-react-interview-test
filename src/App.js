f01304292ff57444e0745a0f5ca7fe3c97873a89
Andrew Shand <andy@webantic.co.uk>
2018-02-09 09:59:58 +0000

    Add jsx boilerplate

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  getDocuments() {
+    return []
+  }
+
+  renderDocument(doc) {
+    return <div className="document-pod">
+      <div className="circle"></div>    
+      <div className="title">{doc.title}</div>
+      <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</div>
+      <div className="bottom">
+        <input type="text" />
+        <div className="button">Submit</div>
+      </div>
+    </div>
+  }
+
   render() {
+    const documents = this.getDocuments()
+
     return (
       <div className="App">
         <header className="App-header">
